e79d8594cd8cb672ba1971d5b29aa9d08bb2bc71
acHQue <Peter.demain.pdm@gmail.com>
2024-03-27 22:52:10 +0000

    Create 0.0-runMeFirst.sh

---
+++ 0.0-runMeFirst.sh
@@ -0,0 +1,9 @@
+./0-download-ELK.sh
+./1-unzip.sh
+./2-downloadInstallNginx.sh
+./3-configure-nginx.sh
+./4-set-services.sh
+./5-config-elasticsearch.sh
+sleep 180
+./6-reset-elasticsearch-password.sh
+./7-startFirefox.sh
